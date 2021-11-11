2fa26c71ab7e581b4f9ea256524a223137557abc
zhonglaogen <zlxzlxxs98@163.com>
2021-11-11 15:39:31 +0800

    change hario

--- src/main/java/com/rd/hcb/Test1.java
+++ src/main/java/com/rd/hcb/Test1.java
@@ -11,6 +11,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Test1 {
 
     public static void main(String[] args) {
+
         ReentrantLock r  = new ReentrantLock();
         r.lock();
         new Thread(() -> {
